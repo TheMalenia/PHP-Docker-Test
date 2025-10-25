7ab2598f68e9e5a69e81cdb809e2aec2fcbb6e45
Hasan <darkhasan05@protonmail.com>
2025-10-25 13:14:59 +0330

    update tests

--- public/index.php
+++ public/index.php
@@ -6,7 +6,6 @@
 // Composer autoloader (PSR-4)
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// Path to routes file (match directory case: Routes)
 $routesFile = __DIR__ . '/../app/Routes/routes.php';
 
 use App\Container\SimpleContainer;
@@ -14,19 +13,10 @@ use App\Container\Services;
 use App\Routes\Router;
 use App\Http\Request;
 
-// -------------------------
-// Bootstrap container & services
-// -------------------------
 $container = new SimpleContainer();
 Services::register($container);
 
-// -------------------------
-// Setup routing
-// -------------------------
 $router = new Router($routesFile, $container);
 
-// -------------------------
-// Build and dispatch request
-// -------------------------
 $request = new Request();
 $router->dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD'], $request);
