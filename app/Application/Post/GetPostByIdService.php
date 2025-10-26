6592a41a1a63b994df1f6009118778a3e2c8996c
Hasan <darkhasan05@protonmail.com>
2025-10-26 22:55:57 +0330

    Change files place

---
+++ app/Application/Post/GetPostByIdService.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Application\Post;
+
+use App\Domain\Repository\PostRepositoryInterface;
+//use App\Domain\Exception\PostNotFoundException;
+
+final class GetPostByIdService
+{
+    public function __construct(
+        private PostRepositoryInterface $posts
+    ) {}
+
+    public function execute(int $id)
+    {
+        $post = $this->posts->getPost($id);
+
+//        if ($post === null) {
+//            throw new PostNotFoundException(sprintf('Post with id %d not found', $id));
+//        }
+
+        return $post;
+    }
+}
